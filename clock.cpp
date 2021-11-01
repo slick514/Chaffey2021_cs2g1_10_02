ab34194467e4ed05c5b6ee61c52d210bd858080a
Justin Gries <justin.gries@gmail.com>
2021-11-01 12:31:30 -0700

    Stubbed out Clock functionality

--- clock.cpp
+++ clock.cpp
@@ -4,16 +4,33 @@
 
 #include "clock.h"
 
-void Clock::setClock(int hr, int min, int sec)
-{
-    hours = hr;
-    minutes = min;
-    seconds = sec;
+/**
+ * TODO:
+    To get the current time, use the following code, which requires that you include the <ctime> header:
+    time_t current_time = time(0);
+    tm* local_time = localtime(&current_time);
+    int hours = local_time->tm_hour;
+    int minutes = local_time->tm_min;
+ */
+int Clock::get_hours(){
+    return 0;
 }
 
-Clock::Clock() {
-    hours = 00;
-    minutes = 00;
-    seconds = 00;
+/**
+ * TODO:
+    To get the current time, use the following code, which requires that you include the <ctime> header:
+    time_t current_time = time(0);
+    tm* local_time = localtime(&current_time);
+    int hours = local_time->tm_hour;
+    int minutes = local_time->tm_min;
+ */
+int Clock::get_minutes(){
+    return 0;
 }
 
+/**
+ * TODO call the get_hours and get_minutes functions to generate this string
+ */
+std::string Clock::get_time() {
+    return std::string();
+}
--- clock.h
+++ clock.h
@@ -5,24 +5,32 @@
 #ifndef CHAFFEY2021_CS2G1_10_02_CLOCK_H
 #define CHAFFEY2021_CS2G1_10_02_CLOCK_H
 
+#include <string>
 
+/**
+ *  Implement a class Clock whose get_hours and get_minutes member functions return the current time at your location.
+ *
+ */
 class Clock
 {
-private:
-    int hours;
-    int minutes;
-    int seconds;
 public:
-    Clock();
 
-    Clock(int hr, int min, int sec)
-    {
-        hours = hr;
-        minutes = min;
-        seconds = sec;
-    }
+    /**
+     * @return a string with the hours and minutes
+     */
+    std::string get_time();
 
-    void setClock(int, int, int);
+    /**
+     *
+     * @return the current time (minutes after the hour) at this location
+     */
+    static int get_hours();
+
+    /**
+     *
+     * @return the current time (hours) at this location
+     */
+    static int get_minutes();
 };
 
 
